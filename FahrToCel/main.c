46a4a69298a66479a11c297e3e3a473aecc15a36
dgoceva <44052125+dgoceva@users.noreply.github.com>
2023-02-06 12:49:49 +0200

    05-02-2023

--- FahrToCel/main.c
+++ FahrToCel/main.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 #define LOWER 0
 #define UPPER 30
 #define STEP 20
+*/
 
 int main()
 {
     float cel;
+    int lower;
+    int upper, step;
+
+ //   scanf("%d%d%d",&lower,&upper,&step);
+    printf("Lower=");
+    scanf("%d",&lower);
+    printf("Upper=");
+    scanf("%d",&upper);
+    printf("Step=");
+    scanf("%d",&step);
+
 //    for (;;)
     printf("Fahr\t\tCel\n");
-    for (int fahr=LOWER;fahr<=UPPER;fahr+=STEP)
+/*
+    for (int fahr=lower;fahr<=upper;fahr+=step)
     {
         cel = (fahr-32)*5/9.;
         printf("%d\t\t%g\n",fahr,cel);
     }
+*/
+    int fahr=lower;
+    // while(1)
+/*    while (fahr<=upper)
+    {
+        cel = 5/9.*(fahr-32);
+        printf("%d\t\t%g\n",fahr,cel);
+        fahr += step;
+    }
+    */
+    do {
+        cel = 5/9.*(fahr-32);
+        printf("%d\t\t%g\n",fahr,cel);
+        fahr += step;
+    } while (fahr<=upper);
 //    printf("Hello world!\n");
 /*    return EXIT_FAILURE;
 */
